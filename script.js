eada60eda5ea06240b07523a42b465f5b3bf2491
Antonio Bradaric <antonio.bradaric@protonmail.com>
2024-05-03 13:15:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
-document.getElementById("greeting").innerText =
-  new Date().getHours() >= 18 ? "Bonsoir 👋" : "Bonjour 👋";
-
-function loadScript(src, dataDomain) {
-  // Check if script is already loaded based on 'src' and 'data-domain'
-  var existingScript = document.querySelector(`script[src="${src}"][data-domain="${dataDomain}"]`);
-  if (existingScript) {
-    return; // Script is already loaded
-  }
-
-  var script = document.createElement("script");
-  script.src = src;
-  script.defer = true;
-  script.setAttribute("data-domain", dataDomain);
-  document.head.appendChild(script);
-}
-
-loadScript("https://plausible.io/js/script.js", "abradaric.me");
+const hour = new Date().getHours();
+document.getElementById("greeting").innerText = hour >= 6 && hour < 18 ? "Bonjour 👋" : "Bonsoir 👋";
